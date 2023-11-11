62011312c49f5cac730e37d8b426b281602ce07d
yifanhao <yifanhao@students.cs.ubc.ca>
2023-11-11 11:36:58 -0800

    get ingredients div

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,13 @@ const app = express()
 const port = 3000
 
 app.get('/', async(req, res) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://www.sephora.com/ca/en/product/sheer-skin-tint-with-hyaluronic-acid-squalane-P501777?skuId=2536183&icid2=products%20grid:p501777:product', { waitUntil: "networkidle2" });
   await page.waitForSelector("h1");
+  const t = await page.$('#ingredients')
   let a = await page.content();
-  console.log(a);
+  console.log(t.textContent);
   await browser.close();
 })
 
