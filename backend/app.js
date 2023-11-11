e490acd7c897221253a2f97bf6cc63c5dc26e30b
yifanhao <yifanhao@students.cs.ubc.ca>
2023-11-11 11:53:14 -0800

    shorten logic

--- backend/app.js
+++ backend/app.js
@@ -10,35 +10,20 @@ app.get('/', async(req, res) => {
   await page.goto('https://www.sephora.com/ca/en/product/sheer-skin-tint-with-hyaluronic-acid-squalane-P501777?skuId=2536183&icid2=products%20grid:p501777:product', { waitUntil: "networkidle2" });
   await page.waitForSelector("h1");
 
-  // Find the element with class 'ingredients'
-  const ingredientsElement = await page.$('#ingredients');
+  const ingredientsText = await page.evaluate(() => {
+    const div = document.querySelector('#ingredients > div:first-child');
 
-  if (ingredientsElement) {
-    // Use page.evaluate to run a function in the context of the page
-    const firstDivInsideIngredients = await ingredientsElement.evaluate(() => {
-      // Find the first div inside the 'ingredients' element
-      const firstDiv = document.querySelector('#ingredients > div:first-child');
-
-      if (firstDiv) {
-        // Return any information you need from the first div (e.g., text content)
-        return {
-          textContent: firstDiv.textContent,
-          // Add more properties as needed
-        };
-      }
-
-      // Return null if the first div is not found
-      return null;
-    });
-
-    if (firstDivInsideIngredients) {
-      console.log('Text Content of the 1st Div inside "ingredients":', firstDivInsideIngredients.textContent);
+    if (div) {
+        return div.textContent;
     } else {
-      console.log('1st Div inside "ingredients" not found.');
+        return null;
     }
-  } else {
-    console.log('Element with class "ingredients" not found.');
+  })
+
+  if (ingredientsText) {
+    console.log(ingredientsText);
   }
+
   let a = await page.content();
   await browser.close();
 })
