a0a0aa078cac1907cf266593517f5f3c66c985c9
SnowGoose0 <jason2580134@outlook.com>
2023-11-11 18:11:28 -0800

    second base

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -4,7 +4,7 @@ import { Second } from './components/Second';
 import './App.css'
 
 function App() {
-  const [activeComponent, setActiveComponent] = useState(1);
+  const [activeComponent, setActiveComponent] = useState(2);
 
   const renderComponent = () => {
     switch (activeComponent) {
--- extension/src/components/First.tsx
+++ extension/src/components/First.tsx
@@ -5,20 +5,12 @@ export const First = () => {
   return (
     <div className="a">
       <div className="div">
-        <div className="frame">
-          <div className="overlap-group">
-            <div className="rectangle" />
-            <p className="ECO-GLOW">
-              <span className="text-wrapper">ECO </span>
-              <span className="span">GLOW</span>
-            </p>
-          </div>
-        </div>
-        <div className="overlap">
+        <div className="rectangle" />
+        <div className="overlap-group">
           <div className="rectangle-2" />
           <div className="rectangle-3" />
-          <div className="div-wrapper">
-            <div className="text-wrapper-2">Analyze</div>
+          <div className="overlap">
+            <div className="text-wrapper">Analyze</div>
           </div>
         </div>
       </div>
