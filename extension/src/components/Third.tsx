a7f7b97c470d2c9489d1fc1a0ad7f7f58c72f3f2
yifanhao <yifanhao@students.cs.ubc.ca>
2023-11-11 20:59:42 -0800

    third

--- extension/src/components/Third.tsx
+++ extension/src/components/Third.tsx
@@ -1,15 +1,26 @@
-import React from "react";
 import "./Third.css";
 
 export const Third = () => {
+  const percentage = 6.7;
+
+  const greenValue = Math.min(255, Math.floor((percentage / 10) * 255));
+  const redValue = 255 - greenValue;
+
+  const textStyle = {
+    color: `rgb(${redValue}, ${greenValue}, 0)`,
+  };
+
   return (
     <div className="main-3">
       <div className = "text-3">
         Sustainability Score
       </div>
       <div className = "speed-3">
-        <div className = "number-3">
-            27
+        <div className = "number-3" style={textStyle}>
+          { percentage }
+          <div className='bar' style={{backgroundColor: `rgba(${redValue}, ${greenValue}, 0, 0.313)`}}>
+            <div className="bar-child" style={{width: (percentage * 10) + "%", backgroundColor: `rgba(${redValue}, ${greenValue}, 0)`}}></div>
+          </div>
         </div>
       </div>
       <div className = "button-3">
